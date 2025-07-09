d37397d692c9a956e6e51c352813e573248c0b29
Mark Hamilton <markhamilton@MacBook-Pro.local>
2025-07-09 14:23:58 -0500

    Fix default export in config, ready for Amplify deploy

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
-// src/config.js
-export const API_URL =
-  process.env.REACT_APP_API_URL ||
-  "https://firstclassglassmirror.us-east-2.elasticbeanstalk.com";
+// File: src/config.js
+
+// You can also pull this from an env var: process.env.REACT_APP_API_BASE_URL
+const API_BASE_URL = 'https://firstclassglassmirror.us-east-2.elasticbeanstalk.com';
+
+export default API_BASE_URL;
